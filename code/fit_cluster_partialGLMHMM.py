9cd6c6e5aec5d140adc776daec4077a130cbbdef
Lenca Cuturela <cuturela@princeton.edu>
2024-09-17 19:01:38 -0400

    refitting partial glm hmm for pTanh=1 with L2 penalty norm on weights = 1 as before for K=1,2

--- code/fit_cluster_partialGLMHMM.py
+++ code/fit_cluster_partialGLMHMM.py
@@ -35,7 +35,7 @@ if ('CSHL049' in subjectsAll):
 
 df = pd.DataFrame(columns=['subject','fold','K']) # in total z=0,159 inclusively per fold
 z = 0
-for K in [3,4,5]:
+for K in [1,2]:
     for subject in subjectsAll:
             for fold in range(splitFolds):
                 df.loc[z, 'subject'] = subject
