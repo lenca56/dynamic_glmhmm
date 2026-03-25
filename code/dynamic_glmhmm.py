079bb8f39a73ef0d7993c2454dff249966749f6b
Lenca Cuturela <cuturela@princeton.edu>
2026-03-25 11:38:58 -0400

    .

--- code/dynamic_glmhmm.py
+++ code/dynamic_glmhmm.py
@@ -460,7 +460,7 @@ class dynamic_GLMHMM():
             # penalty term for size of weights for the standard model
             lf+= L2penaltyW * -1/2 * currentW[:].T @ currentW[:]
 
-        elif model_type in ['partial','dynamic']: # time-varying weights in dynamic GLM-HMM
+        elif model_type in ['partial','dynamic','dynamic-different-prior']: # time-varying weights in dynamic GLM-HMM
 
             if (sigma.sum() != 0): # sigma does not have any 0s
                 # inverse of covariance matrix
